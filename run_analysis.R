f46f1b71125be9af69c895ca8539c7a393a46ab2
jafantonalgo <49726050+jafantonalgo@users.noreply.github.com>
2020-12-31 21:35:05 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -16,11 +16,11 @@ subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = 
 xtest <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
 ytest <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+xtrain <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+ytrain <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 # Step 1: Merges the training and the test sets to create one data set.
-x <- rbind(x_train, x_test)
-y <- rbind(y_train, y_test)
+x <- rbind(xtrain, xtest)
+y <- rbind(ytrain, ytest)
 subject <- rbind(subject_train, subject_test)
 mergedData <- cbind(subject, y, x)
 
